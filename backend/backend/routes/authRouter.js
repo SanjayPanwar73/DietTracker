2f5641ea9a9962167494a27172f000588c1e7912
SanjayPanwar73 <panwarsanjay710@gmail.com>
2026-01-28 10:32:40 +0530

    new changes

--- backend/backend/routes/authRouter.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router(); 
-const { signUp, login , logout } = require('../controllers/authController');
-
-
-router.post('/signup', signUp);
-router.post('/login', login);
-router.post('/logout',logout)
-
-module.exports = router;
