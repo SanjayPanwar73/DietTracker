ea1a15aa0465387324e01e938cccb565577081c6
anuragmandwariya <anuragmandwariya40@gmail.com>
2026-01-28 10:18:11 +0530

    fixed issue

--- backend/backend/routes/profileRouter.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const {createProfile, getProfile} = require('../controllers/profileController');
-const authenticate = require('../middlewares/Authenticate');
-
-router.post('/createProfile', authenticate , createProfile);
-router.get('/getProfile', authenticate , getProfile);
-
-module.exports = router;
